e291962aee490bc602b52ea9dbb4099f42f2501a
Asad Kothawala <asadkothawala97@gmail.com>
2023-10-12 16:07:30 +0530

    removed comments from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,6 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default async function Home() {
-  // const session = await getServerSession(authOptions);
-  // if (!session || !session.user) {
-  //   // redirect
-  //   // console.log("not signed in");
-  //   redirect("/api/auth/signin");
-  // } else {
-  //   // console.log("signed in");
-  // }
-
   return (
     <main className="">
       <Link href={"/overview"}>
