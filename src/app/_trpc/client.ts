01d72940b69bf381b526664dcbe765f79c70d4d3
Asad Kothawala <asadkothawala97@gmail.com>
2023-10-16 11:13:24 +0530

    invalidate trpc queries on mutation

--- src/app/_trpc/client.ts
+++ src/app/_trpc/client.ts
@@ -1,4 +1,13 @@
 import { AppRouter } from "@/server";
 import { createTRPCReact } from "@trpc/react-query";
 
-export const trpcClient = createTRPCReact<AppRouter>({});
+export const trpcClient = createTRPCReact<AppRouter>({
+  overrides: {
+    useMutation: {
+      async onSuccess(opts) {
+        await opts.originalFn();
+        await opts.queryClient.invalidateQueries();
+      },
+    },
+  },
+});
